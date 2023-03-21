f00a2228a4605bc873b25ef165af94f18a191445
sximenez <70152164+sximenez@users.noreply.github.com>
2023-03-21 16:04:56 +0100

    update: 21 03 2023 afternoon

---
+++ example-app/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import './index.css';
+
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
